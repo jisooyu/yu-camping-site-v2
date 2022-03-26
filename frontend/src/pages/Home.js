6cd4a0bf56fcd7824d02b51049e607c3e7e78ea2
jisooyu <jisooyu@hotmail.com>
2022-03-26 12:39:16 +0900

    upto creating camps and adding conditional display into Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,31 @@
 import { FaPencilAlt, FaListAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <>
-      <section className='heading'>
-        <h1>What to you need help with?</h1>
-        <p>Please choose an option from below</p>
-      </section>
-      <Link to='/new-camp' className='btn btn-block'>
-        <FaPencilAlt />
-        Add New Camp Site
-      </Link>
-      <Link to='/camps' className='btn btn-block'>
-        <FaListAlt />
-        View Camp Sites
-      </Link>
+      {user ? (
+        <>
+          <Link to='/new-camp' className='btn btn-block'>
+            <FaPencilAlt />
+            Add New Camp Site
+          </Link>
+          <Link to='/camps' className='btn btn-block'>
+            <FaListAlt />
+            View Camp Sites
+          </Link>
+        </>
+      ) : (
+        <>
+          <section className='heading'>
+            <h1>What to you need help with?</h1>
+
+            <p>Please log in first</p>
+          </section>
+        </>
+      )}
     </>
   );
 };
